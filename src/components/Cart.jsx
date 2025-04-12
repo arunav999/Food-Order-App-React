a307b0cf8a2839c19de9e6208ba1d22d3e06bf75
Arunav Singh <arunavsingh999@gmail.com>
2025-04-12 21:00:45 +0530

    working on cart items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import Modal from "./UI/Modal.jsx";
 import CartContext from "../store/CartContext.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
 import Button from "./UI/Button.jsx";
+import CartItem from "./CartItem.jsx";
 
 import { currencyFormatter } from "../util/formatting.js";
 
@@ -26,9 +27,14 @@ export default function Cart() {
         <h2>Your Cart</h2>
         <ul>
           {cartCtx.items.map((item) => (
-            <li key={item.id}>
-              {item.name} - {item.quantity}
-            </li>
+            <CartItem
+              key={item.id}
+              name={item.name}
+              quantity={item.quantity}
+              price={item.price}
+              onIncrease={() => cartCtx.addItem(item)}
+              onDecrease={() => cartCtx.removeItem(item.id)}
+            />
           ))}
         </ul>
         <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
-export default function CartItem() {
+import { currencyFormatter } from "../util/formatting.js";
+
+export default function CartItem({
+  name,
+  quantity,
+  price,
+  onIncrease,
+  onDecrease,
+}) {
   return (
     <>
-      
+      <li className="cart-item">
+        <p>
+          {name} - {quantity} x {currencyFormatter.format(price)}
+        </p>
+        <p className="cart-item-actions">
+          <button onClick={onDecrease}>-</button>
+          <span>{quantity}</span>
+          <button onClick={onIncrease}>+</button>
+        </p>
+      </li>
     </>
-  )
+  );
 }
